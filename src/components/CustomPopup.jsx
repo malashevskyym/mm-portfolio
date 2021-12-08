7539ddf264a37747f70f07a4cded9ee7ce744e50
malashevskyym <81178491+malashevskyym@users.noreply.github.com>
2021-12-08 14:37:31 -0500

    Attempt4 morePInfo component inside the popoup

--- src/components/CustomPopup.jsx
+++ src/components/CustomPopup.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import popupStyles from "./custom-popup.module.css";
 import ReactDom from 'react-dom'
 import MorePInfo from './MorePInfo';
+import {ExternalLink} from 'react-external-link';
 
 const CustomPopup = (props) => {
   if (!props.show) return null
@@ -13,7 +14,49 @@ const CustomPopup = (props) => {
           &times;
         </span>
         <div className={popupStyles.content}>
-        <MorePInfo name= {props.name} link={props.link} description = {props.description} language = {props.language} tools={props.tools} learned ={props.learned}></MorePInfo>
+        <div className = "mInfo">
+            <div className= "container1">
+                <div className ="L">
+                        <h2 className= "ProjectName">{props.name}</h2>
+                    </div>
+                    <div className= "R">
+                        <ExternalLink href={props.link} className= "github-link">
+                                    <img className='foot-img' src="https://img.icons8.com/fluent/48/000000/github.png" alt="Git Hub" height= '70%'/>
+                        </ExternalLink>
+                    </div>
+                </div>
+                <div className= 'Project'>
+                    <div className="gright">
+                        <ul className="project-description"> {props.description} </ul>
+                    </div>
+                </div>
+                <div className= "ProDetails">
+                    <div className="gleft1">
+                        <div className= "project-details1">
+                            <h3 className="Detail">Language</h3>
+                        </div>
+                    </div>
+                    <div className="gright1">
+                        <p>{props.language}</p>
+                    </div>
+                    <div className="gright2">
+                        <div className= "project-details2">
+                            <h3 className="Detail">Tools</h3>
+                        </div>
+                    </div>
+                    <div className="gleft2">
+                        <p>{props.tools}</p>
+                    </div>
+                    <div className="gleft3">
+                        <div className= "project-details3">
+                        <h3 className="Detail">I loved that I learned...</h3>
+                        </div>
+                    </div>
+                    <div className="gright3">
+                        <p>{props.learned}</p>
+                    </div>
+                </div>
+        </div>
         </div>
       </div>
     </div>, 
