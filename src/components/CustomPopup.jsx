40fad696d73640f5167e0cea907225796110db85
malashevskyym <81178491+malashevskyym@users.noreply.github.com>
2021-12-07 22:56:32 -0500

    Fixed Delay in State change

--- src/components/CustomPopup.jsx
+++ src/components/CustomPopup.jsx
@@ -1,42 +1,23 @@
 import { useEffect, useState } from "react";
 import popupStyles from "./custom-popup.module.css";
-import PropTypes from "prop-types";
+import ReactDom from 'react-dom'
+
 const CustomPopup = (props) => {
-  const [show, setShow] = useState(false);
-
-  const closeHandler = (e) => {
-    setShow(false);
-    props.onClose(false);
-  };
-
-  useEffect(() => {
-    setShow(props.show);
-  }, [props.show]);
-
-  return (
-    <div
-      style={{
-        visibility: show ? "visible" : "hidden",
-        opacity: show ? "1" : "0"
-      }}
-      className={popupStyles.overlay}
-    >
+  if (!props.show) return null
+  return ReactDom.createPortal(
+    <div className={popupStyles.overlay}>
       <div className={popupStyles.popup}>
         <h2>{props.title}</h2>
-        <span className={popupStyles.close} onClick={closeHandler}>
+        <span className={popupStyles.close} onClick={props.onClose}>
           &times;
         </span>
         <div className={popupStyles.content}>{props.children}</div>
       </div>
-    </div>
+    </div>, 
+    document.getElementById('portal')
   );
 };
 
-CustomPopup.propTypes = {
-  title: PropTypes.string.isRequired,
-  show: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired
-};
 
 export default CustomPopup;
 
--- src/components/pages/Coding.js
+++ src/components/pages/Coding.js
@@ -9,35 +9,26 @@ const Coding = () => {
     const [visibility1, setVisibility1] = useState(false);
     const [visibility2, setVisibility2] = useState(false);
     
-    const popupCloseHandler = (e) => {
-        setVisibility(e);
-    };
-    const popupCloseHandler1 = (e) => {
-        setVisibility1(e);
-    };
-    const popupCloseHandler2 = (e) => {
-        setVisibility2(e);
-    };
 
     return (
         <div className= "projectPage">
             <div className="project-grid"> 
-                <button className ="button" onClick={(e) => setVisibility(!visibility)}>
+                <button className ="button" onClick={() => setVisibility(true)}>
                     <ProjectCards className="project" title = 'Parsing Library Capstone' image = 'images/lib2.jpg' bcolor="#D3DADA" />
                 </button>
-                <button className ="button" onClick={(e) => setVisibility1(!visibility1)}>
+                <button className ="button" onClick={() => setVisibility1(true)}>
                     <ProjectCards className="project1" title = 'Portfolio'  image = 'images/lib2.jpg' bcolor="#EBEBEB" />
                 </button>
-                <button className ="button" onClick={(e) => setVisibility2(!visibility2)}>
+                <button className ="button" onClick={() => setVisibility2(true)}>
                     <ProjectCards className="project2" title = 'Music Database Selector' image = 'images/lib2.jpg' bcolor="#CAD9E2"/>
                 </button>
-                <CustomPopup onClose={popupCloseHandler} show={visibility}>
+                <CustomPopup onClose={() => setVisibility(false)} show={visibility}>
                     <MorePInfo name= "Parsing Library Capstone" link="https://github.com/WoffordCollege-COSC410/project-fall-2021-shannon.git" description = "For my capstone project, we used an agile software development framework and test-driven design to create a command line parsing library. We also created demonstrations for different features of the library." language = "Java" tools="Git, Cucumber, Gradle" learned = "I learned the challenges of creating a product from nothing. Working as a team to develop a product using Scrum allowed me to learn how to remain flexible while also delivering the product to the product owner." ></MorePInfo>
                 </CustomPopup>
-                <CustomPopup onClose={popupCloseHandler1} show={visibility1}>
+                <CustomPopup onClose={() => setVisibility1(false)} show={visibility1}>
                     <MorePInfo name= "Portfolio" link="https://github.com/malashevskyym/mm-portfolio.git" description = "Used the React library to create a website demonstrating my projects and background." language = "JavaScript" tools="React" learned = "I learned the fundamentals of the React Library and creating reusable component-based design." ></MorePInfo>
                 </CustomPopup>
-                <CustomPopup onClose={popupCloseHandler2} show={visibility2}>
+                <CustomPopup onClose={() => setVisibility2(false)} show={visibility2}>
                     <MorePInfo name= "Music Database Selector" link="https://github.com/malashevskyym/project4.git" description = "Created a website that retrieved Album data and comments from users for a user elected band from a database." language= "JavaScript" tools="Express, MongoDB, Mongoose" learned="I learned how the back-end and front-end of a website interact to update the website based on user inputs" ></MorePInfo>
                 </CustomPopup>
             </div>
